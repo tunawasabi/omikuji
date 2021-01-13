a0da239ee4b9ee6e5d08339fb2ad89db4a7b49f4
tunawasabi <77270077+tunawasabi@users.noreply.github.com>
2021-01-13 20:07:12 +0900

    役満を追加

--- src/kuji.js
+++ src/kuji.js
@@ -1,7 +1,10 @@
 function hiku() {
-    const r = Math.floor(Math.random() * 100);
+    const r = Math.floor(Math.random() * 100000);
     const result = document.getElementById('result');
-    if (r < 3){
+    
+    if (r == 0){
+        result.innerHTML = '役満！';
+    }else if (r < 3000){
         result.innerHTML = '大吉！';
     }else{
         result.innerHTML = '大凶！';
